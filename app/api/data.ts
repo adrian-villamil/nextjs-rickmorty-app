fa75daea4fcc17054f4d7c111a9bb48258cddb72
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-01-31 23:14:22 -0500

    Adding a new service where I can get a character by its id.

--- app/api/data.ts
+++ app/api/data.ts
@@ -1,7 +1,17 @@
 const BASE_URL = 'https://rickandmortyapi.com/api';
 
-export async function getCharacters(currentPage: string) {
+export async function getAllCharacters(currentPage: string) {
   const res = await fetch(`${BASE_URL}/character/?page=${currentPage}`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data.');
+  }
+
+  return res.json();
+}
+
+export async function getCharacterById(characterId: string) {
+  const res = await fetch(`${BASE_URL}/character/${characterId}`);
 
   if (!res.ok) {
     throw new Error('Failed to fetch data.');
