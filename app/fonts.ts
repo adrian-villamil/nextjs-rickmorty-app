dbc31a3abaa8dc18a293bde79e370e6de995de86
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-02-01 17:53:11 -0500

    New font for titles and subtitles, besides added global css variable colors.

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,5 +1,10 @@
-import { Inter } from 'next/font/google';
+import { Creepster, Inter } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin-ext'],
 });
+
+export const creepster = Creepster({
+  subsets: ['latin'],
+  weight: ['400'],
+})
