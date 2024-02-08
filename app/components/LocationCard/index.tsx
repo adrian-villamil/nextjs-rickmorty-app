1bf0f282d8333c045fd43489c8c9ad26fbca654f
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-02-07 23:06:05 -0500

    Adding icons to every location card depending of their type value.

--- app/components/LocationCard/index.tsx
+++ app/components/LocationCard/index.tsx
@@ -2,7 +2,28 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Location } from '@/app/types/api-types';
 import { creepster } from '@/app/fonts';
+import { IoPlanet } from "react-icons/io5";
+import { FaQuestionCircle, FaUmbrellaBeach } from 'react-icons/fa';
+import { GiGalaxy, GiNightSleep } from "react-icons/gi";
+import { LiaLightbulb, LiaSatelliteSolid } from "react-icons/lia";
+import { LuCircleDot } from 'react-icons/lu';
+import { PiCastleTurretFill, PiCirclesThreeFill, PiGearFineBold, PiTelevisionSimpleBold } from "react-icons/pi";
 import styles from './locationcard.module.css';
+
+const type: Record<string, JSX.Element> = {
+  ['Cluster']: <GiGalaxy />,
+  ['Customs']: <PiGearFineBold />,
+  ['Dimension']: <PiCirclesThreeFill />,
+  ['Dream']: <GiNightSleep />,
+  ['Fantasy town']: <PiCastleTurretFill />,
+  ['Menagerie']: <LiaLightbulb />,
+  ['Microverse']: <LuCircleDot />,
+  ['Planet']: <IoPlanet />,
+  ['Resort']: <FaUmbrellaBeach />,
+  ['Space station']: <LiaSatelliteSolid />,
+  ['TV']: <PiTelevisionSimpleBold />,
+  ['unknown']: <FaQuestionCircle />,
+};
 
 export default function LocationCard({
   location,
@@ -28,14 +49,9 @@ export default function LocationCard({
         </Link>
         <div className={styles['card-info']}>
           <div>
-            <span>Dimension:</span>
-            <span className={`${styles.dimension} ${styles[location.dimension]}`}>
-              {location.dimension}
-            </span>
-          </div>
-          <div>
             <span>Type:</span>
-            <span className={`${styles.status} ${styles[location.type]}`}>
+            <span className={`${styles.type} ${styles[location.type]}`}>
+              {type[location.type]}
               {location.type}
             </span>
           </div>
