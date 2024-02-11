f294655b2207f286d7b5c5cbd404dc1a4228ec1b
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-02-10 20:06:31 -0500

    Adding to inter font different font weights.

--- app/fonts.ts
+++ app/fonts.ts
@@ -2,6 +2,7 @@ import { Creepster, Inter } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin-ext'],
+  weight: ['100','200','300','400','500','600','700','800','900'],
 });
 
 export const creepster = Creepster({
