84db08a9ebec351ce6ee36ce454d2e557431e854
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-02-12 14:52:53 -0500

    Changed the import.

--- app/api/characters.ts
+++ app/api/characters.ts
@@ -1,5 +1,5 @@
 import { ApiResponse, Character } from "../types/api-types";
-import { RICK_AND_MORTY_API_URL } from '../../config';
+import { RICK_AND_MORTY_API_URL } from '@/config';
 
 export async function getAllCharacters(currentPage: string): Promise<ApiResponse<Character[]>> {
   const response = await fetch(`${RICK_AND_MORTY_API_URL}/character/?page=${currentPage}`);
