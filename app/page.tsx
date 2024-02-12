7eb3c098db2a58749c8f866fea64abbbcaefda3d
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-02-11 23:33:41 -0500

    Class for only the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { creepster } from "./fonts";
 export default function Home() {
   return (
     <main>
-      <h1 className={creepster.className}>Rick And Morty</h1>
+      <h1 className={`page-title ${creepster.className}`}>Rick And Morty</h1>
     </main>
   )
 }
