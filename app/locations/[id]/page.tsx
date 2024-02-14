8de3335d930d2d093a86fcf349db2c41289c0014
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-02-13 21:56:50 -0500

    Added a message that will show only when the location has no residents.

--- app/locations/[id]/page.tsx
+++ app/locations/[id]/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { getLocationById } from "@/app/api/locations";
 import { getCharactersFromUrls } from "@/app/api/characters";
+import { BsDashCircleFill } from "react-icons/bs";
 import styles from './location.module.css';
 
 export default async function Page({
@@ -36,31 +37,33 @@ export default async function Page({
         <hr />
         <div className={styles['residents-container']}>
           <h2 className={styles['residents-title']}>Residents</h2>
-          <div className={styles['residents-list']}>
-            {residents.map((resident) => (
-              <div key={resident.id} className={styles['resident-card']}>
-                <Image
-                  src={resident.image}
-                  alt={resident.name}
-                  width={300}
-                  height={300}
-                  priority
-                  className={styles['resident-image']}
-                />
-                <div className={styles['resident-info']}>
-                  <Link
-                    href={`/characters/${resident.id}`}
-                    className={styles['resident-name']}
-                  >
-                    {resident.name}
-                  </Link>
-                  <span className={styles['resident-status']}>
-                    Status: {resident.status}
-                  </span>
+          {location.residents.length === 0 ?
+            <span className={styles['no-residents']}><BsDashCircleFill />There are not residents in this location</span> :
+            <div className={styles['residents-list']}>
+              {residents.map((resident) => (
+                <div key={resident.id} className={styles['resident-card']}>
+                  <Image
+                    src={resident.image}
+                    alt={resident.name}
+                    width={300}
+                    height={300}
+                    priority
+                    className={styles['resident-image']}
+                  />
+                  <div className={styles['resident-info']}>
+                    <Link
+                      href={`/characters/${resident.id}`}
+                      className={styles['resident-name']}
+                    >
+                      {resident.name}
+                    </Link>
+                    <span className={styles['resident-status']}>
+                      Status: {resident.status}
+                    </span>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>}
         </div>
       </div>
     </main>
