15565e9a3e10b7215a86f50f2cb335b6643cddf7
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-02-16 10:11:40 -0500

    Footer at the moment, not finished yet.

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,9 +1,39 @@
+import Link from 'next/link';
 import styles from './footer.module.css';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Footer
+      <p>
+        {'All intellectual property rights associated with the Rick and Morty universe belong to '}
+        <Link
+          href={'https://www.adultswim.com/'}
+          target='_blank'
+          className={styles['link']}
+        >
+          Adult Swim
+        </Link>
+      </p>
+      <p>
+        {'This app utilizes data from '}
+        <Link
+          href={'https://rickandmortyapi.com/'}
+          target='_blank'
+          className={styles['link']}
+        >
+          The Rick and Morty API
+        </Link>
+      </p>
+      <p>
+        {'Created by '}
+        <Link
+          href={'https://github.com/adrian-villamil'}
+          target='_blank'
+          className={styles['link']}
+        >
+          adrian-villamil
+        </Link>
+      </p>
     </footer>
   );
 }
