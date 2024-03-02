f4672b980b89c75dc7c7960802e1497ef9c6a51e
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-03-02 00:01:32 -0500

    Uninstalled react-responsive, didn't fix the problem.

--- app/components/Pagination/index.tsx
+++ app/components/Pagination/index.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import ReactPaginate from "react-paginate";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { HiDotsHorizontal } from "react-icons/hi";
+import ReactPaginate from "react-paginate";
 import styles from './pagination.module.css';
-import { useMediaQuery } from "react-responsive";
 
 export default function Pagination({
   pages,
@@ -15,21 +13,9 @@ export default function Pagination({
   pages: number,
   currentPage: number,
 }) {
-  const [pageRangeDiplayed, setPageRangeDisplayed] = useState(10);
   const { replace } = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const isMax850 = useMediaQuery({ query: '(min-width: 850px)' });
-  const isMin849 = useMediaQuery({ query: '(max-width: 849px)' });
-  const isMin750 = useMediaQuery({ query: '(min-width: 750px)' });
-  const isMin650 = useMediaQuery({ query: '(min-width: 650px)' });
-  const isMin520 = useMediaQuery({ query: '(min-width: 520px)' });
-
-  if (isMax850) setPageRangeDisplayed(10);
-  if (isMin849) setPageRangeDisplayed(9);
-  if (isMin750) setPageRangeDisplayed(7);
-  if (isMin650) setPageRangeDisplayed(5);
-  if (isMin520) setPageRangeDisplayed(3);
 
   const handlePageChange = (data: { selected: number }) => {
     const params = new URLSearchParams(searchParams);
@@ -42,8 +28,6 @@ export default function Pagination({
       <ReactPaginate
         pageCount={pages}
         forcePage={currentPage}
-        pageRangeDisplayed={pageRangeDiplayed}
-        marginPagesDisplayed={0}
         previousLabel={<FaChevronLeft />}
         nextLabel={<FaChevronRight />}
         breakLabel={<HiDotsHorizontal />}
