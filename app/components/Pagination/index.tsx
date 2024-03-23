ea83b9b4667aa43724dc05c76f58e0c3d46b0c50
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-03-23 16:03:20 -0500

    Responsive pagination.

--- app/components/Pagination/index.tsx
+++ app/components/Pagination/index.tsx
@@ -3,6 +3,7 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { HiDotsHorizontal } from "react-icons/hi";
+import { useMediaQuery } from "react-responsive";
 import ReactPaginate from "react-paginate";
 import styles from './pagination.module.css';
 
@@ -17,6 +18,12 @@ export default function Pagination({
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
+  const isMax850 = useMediaQuery({ query: '(width >= 850px)' });
+  const isMin849 = useMediaQuery({ query: '(750px <= width < 850px)' });
+  const isMin750 = useMediaQuery({ query: '(650px <= width < 750px)' });
+  const isMin650 = useMediaQuery({ query: '(520px <= width < 650px)' });
+  const isMin520 = useMediaQuery({ query: '(width < 520px)' });
+
   const handlePageChange = (data: { selected: number }) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', (data.selected + 1).toString());
@@ -25,15 +32,38 @@ export default function Pagination({
 
   return (
     <div className={styles.container}>
-      <ReactPaginate
-        pageCount={pages}
-        forcePage={currentPage}
-        previousLabel={<FaChevronLeft />}
-        nextLabel={<FaChevronRight />}
-        breakLabel={<HiDotsHorizontal />}
-        onPageChange={handlePageChange}
-        containerClassName={styles['paginate-list']}
-      />
+      {isMax850 && <PaginationBar pages={pages} currentPage={currentPage} range={10} handlePageChange={handlePageChange} />}
+      {isMin849 && <PaginationBar pages={pages} currentPage={currentPage} range={9} handlePageChange={handlePageChange} />}
+      {isMin750 && <PaginationBar pages={pages} currentPage={currentPage} range={7} handlePageChange={handlePageChange} />}
+      {isMin650 && <PaginationBar pages={pages} currentPage={currentPage} range={5} handlePageChange={handlePageChange} />}
+      {isMin520 && <PaginationBar pages={pages} currentPage={currentPage} range={3} handlePageChange={handlePageChange} />}
     </div>
+  );
+}
+
+function PaginationBar({
+  pages,
+  currentPage,
+  range,
+  handlePageChange,
+}: {
+  pages: number;
+  currentPage: number;
+  range: number;
+  handlePageChange: (data: {selected: number}) => void;
+}) {
+
+  return (
+    <ReactPaginate
+      pageCount={pages}
+      forcePage={currentPage}
+      pageRangeDisplayed={range}
+      marginPagesDisplayed={0}
+      previousLabel={<FaChevronLeft />}
+      nextLabel={<FaChevronRight />}
+      breakLabel={<HiDotsHorizontal />}
+      onPageChange={handlePageChange}
+      containerClassName={styles['paginate-list']}
+    />
   );
 }
