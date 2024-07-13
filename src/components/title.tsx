bcb794cd02f44b7216f7b225ddc7acf37b0e9743
adrian-villamil <adrian.villamil@unillanos.edu.co>
2024-07-12 20:21:50 -0500

    Just making sure the title color in dark mode is linked with the css variable value.

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export const Title = ({ title }: Props) => {
   return (
-    <h1 className="text-center text-4xl sm:text-5xl font-bold mb-6 dark:text-lime-500">
+    <h1 className="text-center text-4xl sm:text-5xl font-bold mb-6 dark:text-primary">
       {title}
     </h1>
   );
